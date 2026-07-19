8a0a47143d72c877a8b216df89dd7ce3c67371b7
Thowfik-Rahman <Thowfik.rahman@outlook.com>
2026-07-19 13:05:14 +0530

    Time: 17 ms (57.10%) | Memory: 108.1 MB (19.32%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> result = new HashSet<>();
+        for(int num:nums){
+            if(result.contains(num)){
+                return true;
+            }
+            result.add(num);
+        }
+        return false;
+    }
+}
