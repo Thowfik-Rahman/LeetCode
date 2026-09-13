8ddd78160c7ace1e5fa4d8544b8afce5ad837c53
Thowfik-Rahman <Thowfik.rahman@outlook.com>
2026-09-13 22:06:46 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (14.70%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        targetSum -= root.val;
+
+        // Leaf node
+        if (root.left == null && root.right == null) {
+            return targetSum == 0;
+        }
+
+        // Check left subtree
+        if (hasPathSum(root.left, targetSum)) {
+            return true;
+        }
+
+        // Check right subtree
+        return hasPathSum(root.right, targetSum);
+    }
+}
